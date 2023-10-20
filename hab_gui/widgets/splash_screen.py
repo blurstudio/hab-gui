5769472fe6877ca2d8d652387942182601f5936f
JamesB-Blur <berkheimer@blur.com>
2023-10-20 11:13:42 -0700

    Adding functionality for a Splash screen to be used on startup

--- hab_gui/widgets/splash_screen.py
+++ hab_gui/widgets/splash_screen.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore, QtGui, QtTest, QtWidgets
+from PyQt5 import QtGui, QtWidgets
 
 
 class SplashScreen(QtWidgets.QSplashScreen):
@@ -17,13 +17,13 @@ class SplashScreen(QtWidgets.QSplashScreen):
         QtWidgets.QSplashScreen.__init__(self, pixmap)
         self.movie.frameChanged.connect(self.repaint)
 
-    def showEvent(self, event):
+    def showEvent(self, event):  # noqa: N802
         self.movie.start()
 
-    def hideEvent(self, event):
+    def hideEvent(self, event):  # noqa: N802
         self.movie.stop()
 
-    def paintEvent(self, event):
+    def paintEvent(self, event):  # noqa: N802
         painter = QtGui.QPainter(self)
         pixmap = self.movie.currentPixmap()
         self.setMask(pixmap.mask())

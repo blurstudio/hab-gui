4ea7e9e324f25dfd29f5496f4492206b1ab8d12e
Mike Hendricks <mikeh@blur.com>
2023-11-13 18:25:15 -0800

    Add menu button to hab launcher
    - Add refresh to menu so users can force a refresh
    - Move layout configuration into its own method for easy overriding

--- hab_gui/windows/alias_launch_window.py
+++ hab_gui/windows/alias_launch_window.py
@@ -36,7 +36,7 @@ class AliasLaunchWindow(QtWidgets.QMainWindow):
         button_layout=0,
         parent=None,
     ):
-        super(AliasLaunchWindow, self).__init__(parent)
+        super().__init__(parent)
         self.resolver = resolver
         self.verbosity = verbosity
         self.button_wrap_length = button_wrap_length
@@ -60,6 +60,24 @@ class AliasLaunchWindow(QtWidgets.QMainWindow):
             refresh_time = utils.interval(refresh_time)
             logger.debug(f"Setting auto-refresh interval to {refresh_time} seconds")
             self.refresh_timer.start(refresh_time * 1000)
+
+    def apply_layout(self):
+        """Configures the layout of all widgets in this interface."""
+        column_uri_widget = 1 if self.prefs_enabled else 0
+        self.setCentralWidget(self.main_widget)
+        self.layout.addWidget(self.uri_widget, 0, column_uri_widget)
+        self.layout.addWidget(self.menu_button, 0, column_uri_widget + 1)
+        self.layout.addWidget(self.alias_buttons, 1, 0, 1, -1)
+        self.main_widget.setLayout(self.layout)
+
+        # Ensure the tab order is intuitive. This doesn't come for free because
+        # we need to pass uri_widget as an argument to pinned_uris which requires
+        # creating it first and that breaks the default tab ordering.
+        if self.prefs_enabled:
+            self.setTabOrder(self.pinned_uris, self.uri_widget)
+            self.setTabOrder(self.uri_widget, self.menu_button)
+            self.setTabOrder(self.menu_button, self.alias_buttons)
+            self.setTabOrder(self.alias_buttons, self.pinned_uris)
 
     def closeEvent(self, event):  # noqa: N802
         """Saves the currently selected URI on close if prefs are enabled."""
@@ -90,6 +108,11 @@ class AliasLaunchWindow(QtWidgets.QMainWindow):
             "hab_gui_aliases_widget",
             "hab_gui.widgets.alias_button_grid:AliasButtonGrid",
         )
+        # Allows the user to refresh hab configuration in case it has changed.
+        self._cls_menu_button = self.load_entry_point(
+            "hab_gui_menu_button",
+            "hab_gui.widgets.menu_button:MenuButton",
+        )
         # Allows the user to pin commonly used URI's
         self._cls_uri_pin_widget = self.load_entry_point(
             "hab_gui_uri_pin_widget",
@@ -104,7 +127,7 @@ class AliasLaunchWindow(QtWidgets.QMainWindow):
     def init_gui(self, uri=None):
         self.setWindowIcon(utils.Paths.icon("habihat.svg"))
 
-        self.window = QtWidgets.QWidget()
+        self.main_widget = QtWidgets.QWidget()
         self.layout = QtWidgets.QGridLayout()
 
         self.uri_widget = self._cls_uri_widget(
@@ -112,18 +135,21 @@ class AliasLaunchWindow(QtWidgets.QMainWindow):
         )
 
         # If prefs are enabled, insert the Pinned URI widget
-        column_uri_widget = 0
-        prefs_enabled = self.resolver.user_prefs().enabled
+        self.prefs_enabled = self.resolver.user_prefs().enabled
         if self._cls_uri_pin_widget is None:
             # The pinning widget can be disabled by setting the entry point to null
-            prefs_enabled = False
+            self.prefs_enabled = False
 
-        if prefs_enabled:
+        # Create a refresh button
+        self.menu_button = self._cls_menu_button(
+            self.resolver, verbosity=self.verbosity, hab_widget=self
+        )
+
+        if self.prefs_enabled:
             self.pinned_uris = self._cls_uri_pin_widget(
                 self.resolver, self.uri_widget, verbosity=self.verbosity, parent=self
             )
             self.layout.addWidget(self.pinned_uris, 0, 0)
-            column_uri_widget = 1
             self.pinned_uris.uri_widget = self.uri_widget
 
         self.alias_buttons = self._cls_aliases_widget(
@@ -135,11 +161,10 @@ class AliasLaunchWindow(QtWidgets.QMainWindow):
             parent=self,
         )
 
-        self.setCentralWidget(self.window)
-        self.layout.addWidget(self.uri_widget, 0, column_uri_widget)
+        self.apply_layout()
+
+        # Connect signals
         self.uri_widget.uri_changed.connect(self.uri_changed)
-        self.layout.addWidget(self.alias_buttons, 1, 0, 1, -1)
-        self.window.setLayout(self.layout)
 
         # Check for stored URI and apply it as the current text
         if uri is None:
@@ -150,14 +175,6 @@ class AliasLaunchWindow(QtWidgets.QMainWindow):
 
         # Ensure the URI widget has focus by default
         self.uri_widget.setFocus()
-
-        # Ensure the tab order is intuitive. This doesn't come for free because
-        # we need to pass uri_widget as an argument to pinned_uris which requires
-        # creating it first and that breaks the default tab ordering.
-        if prefs_enabled:
-            self.setTabOrder(self.pinned_uris, self.uri_widget)
-            self.setTabOrder(self.uri_widget, self.alias_buttons)
-            self.setTabOrder(self.alias_buttons, self.pinned_uris)
 
     @utils.cursor_override()
     def refresh_cache(self):
